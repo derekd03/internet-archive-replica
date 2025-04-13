0ed6346ca8977103a521b50862b75dfba4ed2b0c
derekd03 <134988441+derekd03@users.noreply.github.com>
2025-04-13 14:47:28 -0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,6 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# The uploads directory should be created in the root of the project
 UPLOAD_FOLDER = os.environ.get("UPLOAD_FOLDER") or os.path.join(BASE_DIR, '..', 'uploads')
